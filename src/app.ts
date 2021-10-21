6d7b0d3cc2b1a815adaca5e4d07056b286b4a35b
karenmrdn <karenmrdn47@gmail.com>
2021-10-21 16:56:12 +0300

    Validation with decorators added

--- src/app.ts
+++ src/app.ts
@@ -125,8 +125,6 @@ const prod1 = new Product("Book", 29);
 const prod2 = new Product("Earbuds", 59);
 
 /* Autobind decorator */
-console.log("____________________________");
-
 function Autobind(
   target: any,
   methodName: string,
@@ -157,3 +155,86 @@ const printer = new Printer();
 
 const button = document.querySelector("button")!;
 button.addEventListener("click", printer.showMessage);
+
+/* Validation with decorators */
+console.log("____________________________");
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProps: string]: string[]; // e.g. ['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    console.error("Invalid input, please try again!");
+    return;
+  }
+  console.log(createdCourse);
+});
